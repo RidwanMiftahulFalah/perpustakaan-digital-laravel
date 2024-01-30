92cab721873dd67f4dcede3609756f69d69eb215
RidwanMiftahulFalah <miftahridwan1101@gmail.com>
2024-01-30 10:35:21 +0700

    first commit

---
+++ app/Models/Book.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Book extends Model {
+  use HasFactory;
+  use HasUuids;
+
+  protected $guarded = ['id'];
+
+  public function author() {
+    return $this->belongsTo(Author::class);
+  }
+}
